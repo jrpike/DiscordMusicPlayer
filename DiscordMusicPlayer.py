7eee75d149b21cc195de878cb964ac7dd7107a22
Jack <japikesphs@gmail.com>
2019-11-23 14:38:15 -0500

    Update MySQL connector

---
+++ DiscordMusicPlayer.py
@@ -0,0 +1,7 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+	host = "localhost",
+	user = "host",
+	passwd = "hostpass123"
+)
--- setup.sql
+++ setup.sql
@@ -1,3 +1,7 @@
+CREATE USER 'host'@'localhost' IDENTIFIED BY 'hostpass123';
+GRANT ALL PRIVILEGES ON * . * TO 'host'@'localhost';
+CREATE DATABASE music_player
+USE music_player
 CREATE TABLE song_queue (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 url VARCHAR(20),
