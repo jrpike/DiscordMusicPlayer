2e7b993414caf746694d34971a16ed29310bf7ee
Jack <japikesphs@gmail.com>
2019-11-29 18:47:59 -0500

    Increased DB functionality

--- DiscordMusicPlayer.py
+++ DiscordMusicPlayer.py
@@ -92,12 +92,21 @@ def add_song(hostname, username, password, url):
 		passwd = password
 	)
 
+	yt = YouTube(url = url)
+
+	title = yt.player_config_args["player_response"]["videoDetails"]["title"]
+	duration = yt.player_config_args["player_response"]["videoDetails"]["lengthSeconds"]
+	thumbnail_url = yt.player_config_args["player_response"]["videoDetails"]["thumbnail"]["thumbnails"][0]["url"].replace("hqdefault", "mqdefault").split("?")[0]
+
+	if duration == 0:
+		return
+
 	db_curr = db_conn.cursor()
 
-	ps = "INSERT INTO song_queue (url, status) VALUES (%s, %s)"
+	ps = "INSERT INTO song_queue (url, status, title, thumbnail_url, duration) VALUES (%s, %s, %s, %s, %s)"
 
 	db_curr.execute("USE music_player")
-	db_curr.execute(ps, (url, 0))
+	db_curr.execute(ps, (url, 0, title, thumbnail_url, duration))
 
 	db_conn.commit()
 	db_curr.close()
